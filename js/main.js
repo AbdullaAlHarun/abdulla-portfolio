fb502f4b3aa8fc41612d075ff141bfbf138f2680
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-06-04 01:40:04 +0200

    Added initial setup + navbar!

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("ul");
+
+let bars = document.querySelector(".header .fa-bars");
+
+bars.addEventListener("click", function () {
+    menu.classList.toggle("show");
+})
