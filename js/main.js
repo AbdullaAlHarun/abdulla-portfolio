1ec5171512d7ab5790f6779fac27787422c7c1c1
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-06-05 13:55:10 +0200

    Added type animation in hero section!

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ let bars = document.querySelector(".header .fa-bars");
 bars.addEventListener("click", function () {
     menu.classList.toggle("show");
 })
+
+var typed=new Typed(".type-job",{
+    strings: ["Frontend Developer", "Web Developer","UX Designer"],
+    typedSpeed: 70,
+    backSpeed: 55,
+    loop: true
+})
