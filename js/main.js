81d6818388d56ad425d06bdbb3787e6e41404086
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-06-10 22:24:03 +0200

    Added Dinamic Contact Form ! This Function, I can Read all contact message in google sheet!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+// Burger Menu Js
 let menu = document.querySelector("ul");
-
 let bars = document.querySelector(".header .fa-bars");
 
 bars.addEventListener("click", function () {
@@ -27,3 +27,21 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+// Contact Form js
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxXzQOUggOolE6pHh8sj4hgVLksFLFp77Wa-3CyeKupmP4CEx2prP6mP72d9o630zV-/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+  
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+          msg.innerHTML = "Message sent successfully"
+          setTimeout(function(){
+              msg.innerHTML = ""
+          },5000)
+          form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
