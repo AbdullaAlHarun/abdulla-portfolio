6384b8768cc76897f081a0263f4e1d778fb998b7
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-06-10 04:05:56 +0200

    Added About me sections!

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ var typed=new Typed(".type-job",{
     backSpeed: 55,
     loop: true
 })
+
+// tablink for about sections 
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
